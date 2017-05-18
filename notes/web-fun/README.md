d9c278e9e01cea7a9f5f1c86d888746c7b186109
Steven Shaw <steven@steshaw.org>
2017-05-18 18:12:55 +1000

    Charles' talk

---
+++ notes/web-fun/README.md
@@ -0,0 +1,1 @@
+Exploring Charles O'Farrell's Lambda Jam talk [Caught in a web of functions](http://blog.charleso.org/lambdajam-web-functions/#1).
---
+++ notes/web-fun/fun.hs
@@ -0,0 +1,34 @@
+#!/usr/bin/env stack
+{-
+  stack script --resolver lts-8.8
+    --package bytestring
+    --package http-types
+    --package wai
+    --package warp
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.ByteString.Lazy
+import Network.HTTP.Types.Status
+import Network.Wai
+import Network.Wai.Handler.Warp (runEnv)
+
+-- type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+
+app :: Application
+app req respond =
+  respond $ case (requestMethod req, pathInfo req) of
+    ("GET", []) ->
+      html status200 "<h1>Welcome</h1>"
+    ("GET", ["login"]) ->
+      html status200 "<h1>Please login</h1>"
+    _ -> 
+      html status404 "<h1>Not found</h1>"
+
+html :: Status -> ByteString -> Response
+html status body =
+  responseLBS status [("Content-Type", "text/html")] body
+
+main :: IO ()
+main = runEnv 3000 app
