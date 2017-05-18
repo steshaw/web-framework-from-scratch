74a6d4a2ef1963f082635d41b747e0b3b3f167df
Steven Shaw <steven@steshaw.org>
2017-05-18 16:26:22 +1000

    Still allows a "bad app" that doesn't call the continuation

---
+++ notes/resources-laziness-and-cps/bad-app.hs
@@ -0,0 +1,44 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-8.8 --package base
+
+import Control.Exception.Base (bracket)
+import Debug.Trace (trace)
+
+-- | Our dummy requests and responses.
+data Request = Request
+data Response = Response
+
+-- | Our dummy resource.
+data SomeResource = SomeResource
+
+-- | Our modified @App@ type.
+type App = Request -> (Response -> IO ()) -> IO ()
+
+runApp :: App -> IO ()
+runApp app = do
+  app Request $ \ response ->
+    case response of
+      Response -> putStrLn "Sending response"
+
+-- | This app is bad because it doesn't call 'cb'.
+myBadApp :: App
+myBadApp _request _cb = do
+  putStrLn "Responding to request"
+  return ()
+
+{-
+myApp :: App
+myApp _request cb = bracket createResource destroyResource respond
+  where
+    createResource = do
+      putStrLn "Creating resource"
+      return SomeResource
+    destroyResource _ = do
+      putStrLn "Destroying resource"
+    respond SomeResource = do
+      putStrLn "Responding to request" -- A new print statement
+      cb (trace "EVALUATING RESPONSE" Response)
+-}
+
+main :: IO ()
+main = runApp myBadApp
